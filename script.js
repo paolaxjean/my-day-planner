387a688b20439b72553bfd4e4cacc3d6ceb27291
paolaxjean <pr1008631@gmail.com>
2022-06-17 14:47:13 -0700

    updated date and time display

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 var currentDateEl = $('#current-date');
 
 function displayDate() {
-    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+    var rightNow = moment().format('dddd: MMM DD, YYYY');
  currentDateEl.text(rightNow);
-  }
+}
 
+function loadTasks() {
+    // check if localStorage has any tasks
+    // if not then return
+    if (localStorage.getItem("tasks") == null) return;
+}
 
 setInterval(displayDate,1000);
-
